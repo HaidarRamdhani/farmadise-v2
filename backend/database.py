ec8acbcf6250f60690859e7aeb0c17aee9715eb5
Haidar Ramdhani <159707550+HaidarRamdhani@users.noreply.github.com>
2025-03-27 00:24:47 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@localhost:5432/farmadise"
+# URL PostgreSQL (ganti dengan URL database Anda)
+DATABASE_URL = "postgresql://postgres:umyEkSjrtFEFAkAcWthrmKtEjrutlXGd@postgres.railway.internal:5432/railway"
 
+# Inisialisasi engine
 engine = create_engine(DATABASE_URL)
+
+# Inisialisasi SessionLocal
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Inisialisasi Base
 Base = declarative_base()
