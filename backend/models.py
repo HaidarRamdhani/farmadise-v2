81d1ea32eb0025d4a48ac6c4779cbe187ab6ab60
Haidar Ramdhani <159707550+HaidarRamdhani@users.noreply.github.com>
2025-03-27 11:27:58 +0800

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,15 +3,15 @@ from database import Base  # Import Base dari database.py
 
 class Response(Base):
     __tablename__ = "responses"
-    id = Column(Integer, primary_key=True)
-    tanggal = Column(DateTime)
-    jawaban = Column(String)
-    sentimen_negatif = Column(Integer)
-    username = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    tanggal = Column(DateTime, nullable=False)
+    jawaban = Column(String(255), nullable=False)
+    sentimen_negatif = Column(Integer, nullable=False)
+    username = Column(String(50), nullable=False)
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    password_hash = Column(String)
-    role = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    role = Column(String(20), nullable=False)
